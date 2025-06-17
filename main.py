76b8568560d57ce00522f52463f428be05650c1f
NoSleepFox <SimenCodemaster@proton.me>
2025-06-17 20:10:40 +0200

    Fixed stuff

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality
 # import smtplib
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+SECRET_KEY = os.environ["SECRET_KEY"]
+DB_URI = os.environ["DB_URI"]
 
 '''
 Make sure the required packages are installed: 
@@ -29,7 +34,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = SECRET_KEY
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -56,7 +61,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -297,4 +302,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
